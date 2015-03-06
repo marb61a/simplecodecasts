d28bb8a05b52cb84c51f49a805becc092cae574b
Martin Byrne <marb61a@gmail.com>
2015-03-06 09:45:49 +0000

    Added contact page validation and bootstrap messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     
     if @contact.save
-      redirect_to new_contact_path, notice: "Message sent."
+      flash[:success] = 'Message Sent.'
+      redirect_to new_contact_path
     else
-      redirect_to new_contact_path, notice: "Error occured."
+      flash[:danger] = 'An error has occured, message not sent.'
+      redirect_to new_contact_path
     end
   end
   
