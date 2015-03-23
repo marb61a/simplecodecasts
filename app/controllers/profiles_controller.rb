843afaf7bce3085e5dcaf3652dc9ce41577a8ab0
Martin Byrne <marb61a@gmail.com>
2015-03-23 09:47:50 +0000

    Added filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,12 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user!
+  
   def new
     @user = User.find( params[:user_id])
     @profile = Profile.new
   end
+  
   def create 
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
@@ -12,10 +16,33 @@ class ProfilesController < ApplicationController
     else
       render action: :new
     end
- end
+   end
   
   def edit
     @user = User.find( params[:user_id] )
     @profile = @user.profile
   end
+  
+  def update
+     @user = User.find( params[:user_id] )
+     @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
+    end
+  end 
+  
+  private
+  
+  def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
+  
+  def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+  end
+ 
 end
