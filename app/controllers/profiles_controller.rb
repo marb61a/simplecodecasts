27fb2b481e81d823007db744b978660eb576d600
Martin Byrne <marb61a@gmail.com>
2015-03-23 09:33:31 +0000

    Added form partial and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,19 @@ class ProfilesController < ApplicationController
     @user = User.find( params[:user_id])
     @profile = Profile.new
   end
+  def create 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
+    end
+ end
+  
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
 end
